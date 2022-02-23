a974e014c89d21ce7013f8a81932a31b322fa3e0
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-23 10:03:46 -0500

    Init

---
+++ src/test/java/co/com/pattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
