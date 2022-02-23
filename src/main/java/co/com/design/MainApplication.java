af72bd6eeecb7f1edc229de814f4db6aaea35ad1
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-23 17:26:19 -0500

    Behavioral Command

--- src/main/java/co/com/design/MainApplication.java
+++ src/main/java/co/com/design/MainApplication.java
@@ -1,13 +1,13 @@
 package co.com.design;
 
-
+import co.com.pattern.Invoker;
 import co.com.pattern.model.Account;
-import co.com.pattern.proxy.AccountProxy;
-import co.com.pattern.service.IAccount;
+import co.com.pattern.operation.impl.OperationDeposit;
+import co.com.pattern.operation.impl.OperationWithdrawals;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Mono;
+
 
 @SpringBootApplication
 public class MainApplication implements CommandLineRunner {
@@ -18,25 +18,16 @@ public class MainApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Mono<Account> accountMono = Mono.just(
-				Account.builder().id(1).client("Sergio Stives Barrios Buitrago").money(100.000D).build()
-		);
+		Account account = Account.builder().id(1).money(100.000D).build();
 
-		IAccount account = new AccountProxy();
+		OperationDeposit operationDeposit = new OperationDeposit(account, 200.000D);
+		OperationWithdrawals operationWithdrawals = new OperationWithdrawals(account, 100.000D);
 
-		accountMono.doOnNext(accountNew -> account.showBalance(Mono.just(accountNew)))
-						.subscribe();
+		Invoker invoker = new Invoker();
 
-		accountMono
-				.doOnNext(accountNew -> System.out.println("Deposit: 200.000"))
-				.flatMap(accountNew -> account.depositMoney(Mono.just(accountNew), 200.000D))
-				.doOnNext(accountNew -> account.showBalance(Mono.just(accountNew)))
-				.subscribe();
+		invoker.add(operationDeposit);
+		invoker.add(operationWithdrawals);
 
-		accountMono
-				.doOnNext(accountNew -> System.out.println("Withdrawals: 100.000"))
-				.flatMap(accountNew -> account.withdrawals(Mono.just(accountNew), 100.000D))
-				.doOnNext(accountNew -> account.showBalance(Mono.just(accountNew)))
-				.subscribe();
+		invoker.perform();
 	}
 }
